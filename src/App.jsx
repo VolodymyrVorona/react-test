952b79b8445fc32dc5b6ba6be1864ab0c9e11db5
RavenVor <volodimir.vorona@gmail.com>
2021-02-26 23:50:19 +0200

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,50 @@
 import Section from './components/Section/Section';
-// import PaingingList from './components/PaingingList';
-// import Colorpicker from './components/ColorPicker/ColorpPicker';
-// import Alert from './components/Alert/Alert';
-// import Container from './components/Container/Container';
-// import paintings from './paintings.json';
-import Counter from './components/Counter/Counter';
+import PaingingList from './components/PaingingList';
+import Colorpicker from './components/ColorPicker/ColorpPicker';
+import Alert from './components/Alert/Alert';
+import Container from './components/Container/Container';
+import paintings from './paintings.json';
+import Counter from './components/Counter';
 import Dropdown from './components/Dropdown/Dropdown';
 
-// const ColorPickerOptions = [
-//   { lable: 'red', color: '#F44336' },
-//   { lable: 'green', color: '#4CAF50' },
-//   { lable: 'blue', color: '#2196F3' },
-//   { lable: 'grey', color: '#607D8B' },
-//   { lable: 'pink', color: '#E91E63' },
-//   { lable: 'indigo', color: '#3F51B5' },
-// ];
+const ColorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
 
 const App = () => {
   return (
     <>
       <Section title="Состояние компонента">
-        <Counter />
+        <Container>
+          <Counter />
+        </Container>
       </Section>
-
       <Section title="Dropdown">
         <Dropdown />
       </Section>
-
-      {/* <Section title="Alert">
+      <Section>
+        <Container>
+          <Colorpicker options={ColorPickerOptions} />
+        </Container>
+      </Section>
+      <Section title="Alert">
         <Container>
           <Alert text="Шеф, все пропало!" type="success" />
           <Alert text="Шеф, все пропало!" type="warning" />
           <Alert text="Шеф, все пропало!" type="error" />
         </Container>
       </Section>
-
-      <Section title="Color Picker">
-        <Container>
-          <Colorpicker options={ColorPickerOptions} />
-        </Container>
-      </Section>
-
       <Section title="Paintings">
         <Container>
           <PaingingList items={paintings} />
         </Container>
-      </Section> */}
+      </Section>
+      */
     </>
   );
 };
--- src/components/ColorPicker/ColorpPicker.jsx
+++ src/components/ColorPicker/ColorpPicker.jsx
@@ -1,19 +1,51 @@
-import s from './ColorPicker.module.css';
+import React, { Component } from 'react';
+import './ColorPicker.css';
 
-function Colorpicker({ options }) {
-  return (
-    <div className={s.container}>
-      <div>
-        {options.map(option => (
-          <span
-            key={option.color}
-            className={s.option}
-            style={{ backgroundColor: option.color }}
-          ></span>
-        ))}
+class Colorpicker extends Component {
+  state = {
+    actieOptionIdx: 0,
+  };
+
+  setActiveIdx = index => {
+    this.setState({ actieOptionIdx: index });
+  };
+
+  makeOptionClassName = index => {
+    const { actieOptionIdx } = this.state;
+    const optionClasses = ['ColorPicker__option'];
+
+    if (index === actieOptionIdx) {
+      optionClasses.push('ColorPicker__option--active');
+    }
+
+    return optionClasses.join(' ');
+  };
+
+  render() {
+    const { actieOptionIdx } = this.state;
+    const { options } = this.props;
+    const { label } = options[actieOptionIdx];
+
+    return (
+      <div className="ColorPicker">
+        <h2 className="ColorPicker__title">Color Picker</h2>
+        <p>Выбран цвет: {label}</p>
+
+        <div>
+          {options.map(({ label, color }, index) => (
+            <span
+              key={label}
+              className={this.makeOptionClassName(index)}
+              style={{
+                backgroundColor: color,
+              }}
+              onClick={() => this.setActiveIdx(index)}
+            ></span>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Colorpicker;
